077b8602eba54a41cc771656debce9e57694f50f
SobirovI <88648901+SobirovI@users.noreply.github.com>
2021-10-05 19:20:55 +0500

    colors

---
+++ lib/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static HexColor orange = HexColor('#EF7F1A');
+  static HexColor blue = HexColor('#0074C9');
+  static HexColor green = HexColor('#35DC3C');
+  static HexColor red = HexColor('#DC3545');
+  static HexColor pink = HexColor('#FFF5E4');
+}
