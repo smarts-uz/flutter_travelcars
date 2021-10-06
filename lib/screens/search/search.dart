24fef6ee8ed23ddf8f457d010511f12882d67500
SobirovI <88648901+SobirovI@users.noreply.github.com>
2021-10-06 12:37:41 +0500

    three sized box lists

--- lib/screens/search/search.dart
+++ lib/screens/search/search.dart
@@ -15,7 +15,14 @@ class _SearchScreenState extends State<SearchScreen> {
     "One way",
     "Round trip",
   ];
-  int _radioVal = 0;
+
+  List<String> sort = [
+    "By price",
+    "By capacity"
+  ];
+  int? _radioVal1;
+  int? _radioVal2;
+  RangeValues _currentRangeValues = const RangeValues(10, 500);
 
   String? SelectedVal1;
   String? SelectedVal2;
@@ -34,6 +41,83 @@ class _SearchScreenState extends State<SearchScreen> {
 
   DateTime? _selectedDate1 = DateTime.now();
   DateTime? _selectedDate2 = DateTime.now();
+
+  final number_controller = TextEditingController();
+
+  List<Map<String, dynamic>> autoTypes = [
+    {
+      "text": "Cars",
+      "check_box": false
+    },
+    {
+      "text": "Microbus",
+      "check_box": false
+    },{
+      "text": "Midibus",
+      "check_box": false
+    },{
+      "text": "Bus",
+      "check_box": false
+    },
+    {
+      "text": "VIP - auto",
+      "check_box": false
+    },
+  ];
+
+  List<Map<String, dynamic>> autoOptions = [
+    {
+      "text": "Air conditioning",
+      "check_box": false
+    },
+    {
+      "text": "Mikrofon",
+      "check_box": false
+    },{
+      "text": "Fridge",
+      "check_box": false
+    },{
+      "text": "Tv",
+      "check_box": false
+    },
+    {
+      "text": "4WD",
+      "check_box": false
+    },
+    {
+      "text": "First aid kit",
+      "check_box": false
+    },
+    {
+      "text": "Airbags",
+      "check_box": false
+    },
+    {
+      "text": "Fire extinguisher",
+      "check_box": false
+    },
+    {
+      "text": "Plumbing cabin",
+      "check_box": false
+    },
+  ];
+
+  List<Map<String, dynamic>> tarif = [
+    {
+      "text": "Car delivery to a convenient place",
+      "check_box": false
+    },
+    {
+      "text": "Fuel cost",
+      "check_box": false
+    },{
+      "text": "Driver nutrition",
+      "check_box": false
+    },{
+      "text": "Parking payments",
+      "check_box": false
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -77,10 +161,10 @@ class _SearchScreenState extends State<SearchScreen> {
                       flex: 1,
                       child: Radio<int>(
                         value: index,
-                        groupValue: this._radioVal,
+                        groupValue: this._radioVal1,
                         onChanged: (int? value) {
                           if (value != null) {
-                            setState(() => this._radioVal = value);
+                            setState(() => this._radioVal1 = value);
                             print(value);
                           }},
                       ),
@@ -224,16 +308,217 @@ class _SearchScreenState extends State<SearchScreen> {
             Container(
               width: double.infinity,
               height: 55,
-              padding: EdgeInsets.only(left: 6),
+              padding: EdgeInsets.only(left: 15),
               margin: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
               decoration: BoxDecoration(
                   border: Border.all(color: Colors.grey),
                   borderRadius: BorderRadius.circular(5)
               ),
+              child: TextFormField(
+                autovalidateMode: AutovalidateMode.always,
+                decoration: const InputDecoration(
+                  hintText: "Quantity of passengers",
+                ),
+                controller: number_controller,
+                keyboardType: TextInputType.text,
+                cursorColor: Colors.black,
+                style: TextStyle(
+                  fontSize: 20
+                ),
+                expands: false,
+                maxLines: 1,
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 15, top: 10, bottom: 10),
+              child: Text(
+                'Sort by result',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [0, 1].map((int index) => Container(
+                alignment: Alignment.centerLeft,
+                height: 40,
+                width: double.infinity,
+                //padding: EdgeInsets.all(12),
+                child: Row(
+                  children: [
+                    Radio<int>(
+                      value: index,
+                      groupValue: this._radioVal2,
+                      onChanged: (int? value) {
+                        if (value != null) {
+                          setState(() => this._radioVal2 = value);
+                          print(value);
+                        }},
+                    ),
+                    Text(
+                      sort[index],
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              )).toList(),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 15, top: 10, bottom: 10),
+              child: Text(
+                'Price',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 15, bottom: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                      "10",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: RangeSlider(
+                      values: _currentRangeValues,
+                      min: 10,
+                      max: 500,
+                      divisions: 10,
+                      labels: RangeLabels(
+                        _currentRangeValues.start.round().toString(),
+                        _currentRangeValues.end.round().toString(),
+                      ),
+                      onChanged: (RangeValues values) {
+                        setState(() {
+                          _currentRangeValues = values;
+                        });
+                        },
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                      "500",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 15, top: 15, bottom: 5),
+              child: Text(
+                'Auto types',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 5, bottom: 5),
+              height: 150, //autoTypes.length * 10,
+              width: double.infinity,
+              child: ListBox(autoTypes)
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 15, top: 15, bottom: 5),
+              child: Text(
+                'Options',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.only(left: 5, bottom: 5),
+                height: 150, //autoTypes.length * 10,
+                width: double.infinity,
+                child: ListBox(autoOptions)
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 15, top: 15, bottom: 5),
+              child: Text(
+                'Included in type',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.only(left: 5, bottom: 5),
+                height: 150, //autoTypes.length * 10,
+                width: double.infinity,
+                child: ListBox(tarif)
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class ListBox extends StatefulWidget {
+
+  final List<Map<String, dynamic>> boxes;
+
+  ListBox(@required this.boxes);
+  @override
+  _ListBoxState createState() => _ListBoxState();
+}
+
+class _ListBoxState extends State<ListBox> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: widget.boxes.length,
+        itemBuilder: (context, index) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Checkbox(
+                onChanged: (bool? value) {
+                  if (value != null) {
+                    setState(() => widget.boxes[index]["check_box"] = value);
+                  }
+                },
+                value: widget.boxes[index]["check_box"],
+              ),
+              Text(
+                "${widget.boxes[index]["text"]}",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          );
+        }
+        );
   }
 }
