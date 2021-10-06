dd33501d6c376779760d8ad02f04c0d8b2e0ff12
Mukhammadjon <toshboltayevmuxammadjon@gmail.com>
2021-10-06 10:42:28 +0500

    splash screen

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:travelcars/screens/home/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _navigatorHome();
+  }
+  
+  _navigatorHome() async{
+    await Future.delayed(Duration(milliseconds: 1500),(){});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=>HomeScreen()));
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 184, bottom: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 194,
+              width: 232,
+              child: Image.asset("assets/images/logo.png"),
+            ),
+            Container(
+              child: Text(
+                "2018 - 2021 Travel Cars\n Все права защищены",
+                style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    fontFamily: 'Poppins',
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
