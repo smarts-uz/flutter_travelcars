572cb09de027a6f4fcda0f598c28ce64ad6c4f18
SobirovI <88648901+SobirovI@users.noreply.github.com>
2021-10-09 15:42:07 +0500

    Trip item screen done

--- lib/screens/trip/trip_item.dart
+++ lib/screens/trip/trip_item.dart
@@ -11,8 +11,18 @@ class TripItem extends StatefulWidget {
 
 class _TripItemState extends State<TripItem> {
 
+  List<TextEditingController> controllers = [
+    for (int i = 0; i < 4; i++)
+      TextEditingController()
+  ];
+  List<String> hints = ["Name", "E-mail", "Phone", "Write a comment"];
+
+
+
+
   @override
   Widget build(BuildContext context) {
+    double size_h = MediaQuery.of(context).size.height * .25;
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -85,7 +95,8 @@ class _TripItemState extends State<TripItem> {
               ),
             ),
             Container(
-              padding: EdgeInsets.only(top: 6, left: 13),
+              padding: EdgeInsets.only(top: 7, left: 10, right: 10),
+              margin: EdgeInsets.only(top: 15, left: 13, right: 13),
               height: 60,
               color: Color.fromRGBO(255, 250, 241, 1),
               alignment: Alignment.centerLeft,
@@ -144,6 +155,218 @@ class _TripItemState extends State<TripItem> {
                 ],
               )
             ),
+            Container(
+              height: size_h,
+              width: double.infinity,
+              padding: EdgeInsets.only(top: 20, left: 17, right: 17, bottom: 5),
+              child: Stack(
+                children: [
+                  Container(
+                    color: Color.fromRGBO(239, 127, 26, 1),
+                  ),
+                  Positioned(
+                    top: size_h * .05,
+                    left: 15,
+                    child: Icon(
+                      Icons.email,
+                      color: Colors.white,
+                      size: 85,
+                    ),
+                  ),
+                  Positioned(
+                      left: 15,
+                      bottom: size_h * .2,
+                      child: Text(
+                        "Book now",
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.white
+                        ),
+                      )
+                  ),
+                  Positioned(
+                      left: 15,
+                      bottom: size_h * .05,
+                      child: Text(
+                        "We will be glad to receive your order!",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                            color: Colors.white
+                        ),
+                      )
+                  )
+                ],
+              ),
+            ),
+            /*Container(
+              width: double.infinity,
+              height: 50,
+              padding: EdgeInsets.only(left: 15),
+              margin: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5)
+              ),
+              child: TextFormField(
+                autovalidateMode: AutovalidateMode.always,
+                decoration: const InputDecoration(
+                  hintText: "Name",
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 0,
+                    ),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 0,
+                    ),
+                  ),
+                ),
+                controller: name_controller,
+                cursorColor: Colors.black,
+                style: TextStyle(
+                    fontSize: 20
+                ),
+                expands: false,
+                maxLines: 1,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              padding: EdgeInsets.only(left: 15),
+              margin: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5)
+              ),
+              child: TextFormField(
+                autovalidateMode: AutovalidateMode.always,
+                decoration: const InputDecoration(
+                  hintText: "E-mail",
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 0,
+                    ),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 0,
+                    ),
+                  ),
+                ),
+                controller: name_controller,
+                cursorColor: Colors.black,
+                style: TextStyle(
+                    fontSize: 20
+                ),
+                expands: false,
+                maxLines: 1,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              padding: EdgeInsets.only(left: 15),
+              margin: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5)
+              ),
+              child: TextFormField(
+                autovalidateMode: AutovalidateMode.always,
+                decoration: const InputDecoration(
+                  hintText: "Phone",
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 0,
+                    ),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 0,
+                    ),
+                  ),
+                ),
+                controller: name_controller,
+                cursorColor: Colors.black,
+                style: TextStyle(
+                    fontSize: 20
+                ),
+                expands: false,
+                maxLines: 1,
+              ),
+            ),*/
+            Column(
+              children: [0, 1, 2, 3].map((e) => Container(
+                width: double.infinity,
+                height: e == 3 ? 150 : 50,
+                padding: EdgeInsets.only(left: 15),
+                margin: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5)
+                ),
+                child: TextFormField(
+                  keyboardType: e == 2 ? TextInputType.number : TextInputType.text,
+                  autovalidateMode: AutovalidateMode.always,
+                  decoration: InputDecoration(
+                    hintText: hints[e],
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 0,
+                      ),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 0,
+                      ),
+                    ),
+                  ),
+                  controller: controllers[e],
+                  cursorColor: Colors.black,
+                  style: TextStyle(
+                      fontSize: 20
+                  ),
+                  expands: false,
+                  maxLines: e == 3 ? 3 : 1,
+                ),
+              ),).toList(),
+            ),
+            Container(
+              height: 50,
+              width: double.infinity,
+              padding: EdgeInsets.only(top: 5, bottom: 5, left: 15, right: 15),
+              margin: EdgeInsets.only(top: 15),
+              child: RaisedButton(
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                  color: Color.fromRGBO(0, 116, 201, 1),
+                  padding: EdgeInsets.all(8),
+                  textColor: Colors.white,
+                  child: Text(
+                    "Search",
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: () {
+                    print("Name: ${controllers[0].text}");
+                    print("E-mail: ${controllers[1].text}");
+                    print("Phone: ${controllers[2].text}");
+                    print("Comment: ${controllers[3].text}");
+                  }
+              ),
+            )
           ],
         ),
       ),
