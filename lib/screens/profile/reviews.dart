ac88abb61746eb7edfe64f0c9b5fc09e410bd26e
Muhammadsaid22 <82633656+Muhammadsaid22@users.noreply.github.com>
2021-10-13 18:09:02 +0500

    reviews screen rate

--- lib/screens/profile/reviews.dart
+++ lib/screens/profile/reviews.dart
@@ -8,65 +8,258 @@ class Reviews extends StatefulWidget {
 }
 
 class _ReviewsState extends State<Reviews> {
-  List<Map<String, dynamic>> reviews = [
-    {
-      'overallrate': '10',
-      'rate': [
-        {
-          'Punctuality': '10',
-          'Car driving': '10',
-          'knowledge': '10',
-          'Terrain orientation': '10',
-          'language': '10',
-          'Cleanliness / odor in the cabin': '10',
-          'Amenities in the salon': '10',
-          'Driver professionalism': '10',
-          'Price-quality ratio': '10',
-        }
-      ],
-
-    'info':[
+  Map<String, dynamic> reviews = {
+    'overallrate': '10',
+    'rateN': '6',
+    'rate': [
+      {
+        'title': 'Punctuality',
+        "score": 8.0
+      },
+      {
+        'title': 'Car driving',
+        "score": 3.0
+      },
+      {
+        'title': 'Knowledge',
+        "score": 7.0
+      },
+      {
+        'title': 'Punctuality',
+        "score": 6.0
+      },
+      {
+        'title': 'Car driving',
+        "score": 9.0
+      },
+      {
+        'title': 'Knowledge',
+        "score": 9.0
+      },
+      {
+        'title': 'Knowledge',
+        "score": 5.0
+      },
+      {
+        'title': 'Punctuality',
+        "score": 8.0
+      },
+      {
+        'title': 'Car driving',
+        "score": 3.0
+      },
+      {
+        'title': 'Knowledge',
+        "score": 7.0
+      },
+      {
+        'title': 'Punctuality',
+        "score": 6.0
+      },
+      {
+        'title': 'Car driving',
+        "score": 5.0
+      },
+      {
+        'title': 'Knowledge',
+        "score": 5.0
+      },
+      {
+        'title': 'Knowledge',
+        "score": 5.0
+      },
+    ],
+    "reviews": [
       {
         'avatar': '',
-        'name': 'Nazira',
-        'flag': '',
-        'nationality': 'Uzbekistan',
-        'location': 'Family garden, Хумсон',
-        'date': '31-08-2021',
-        'reviewtime': '30 Aug 2021 15:08:52',
-        'fine': 'I would like to note that we have already contacted you more than once.\ n'
-            ' The trip went well. The minibus is comfortable, \n'
-            'clean, without foreign odors (there was even a bakhur for aromatization). \n'
-            'The driver was extremely polite (he opened the doors, arrived at the agreed time,\n'
-            ' immediately offered to connect to bluetooth (if you need musical accompaniment on the road). \n'
-            'He orients himself perfectly in the area.\n'
-            ' We arrived comfortably, quickly and without any outside chatter from the driver. Thank you.\n'
-            ' We will be contact again!'
-      }
+          'name': 'Nazira',
+          'flag': '',
+          'nationality': 'Uzbekistan',
+          'location': 'Family garden, Хумсон',
+          'date': '31-08-2021',
+          'reviewtime': '30 Aug 2021 15:08:52',
+          'fine': 'I would like to note that we have already contacted you more than once.\ n'
+              ' The trip went well. The minibus is comfortable, \n'
+              'clean, without foreign odors (there was even a bakhur for aromatization). \n'
+              'The driver was extremely polite (he opened the doors, arrived at the agreed time,\n'
+              ' immediately offered to connect to bluetooth (if you need musical accompaniment on the road). \n'
+              'He orients himself perfectly in the area.\n'
+              ' We arrived comfortably, quickly and without any outside chatter from the driver. Thank you.\n'
+              ' We will be contact again!'
+        },
+      {
+          'avatar': '',
+          'name': 'Nazira',
+          'flag': '',
+          'nationality': 'Uzbekistan',
+          'location': 'Family garden, Хумсон',
+          'date': '31-08-2021',
+          'reviewtime': '30 Aug 2021 15:08:52',
+          'fine': 'I would like to note that we have already contacted you more than once.\ n'
+              ' The trip went well. The minibus is comfortable, \n'
+              'clean, without foreign odors (there was even a bakhur for aromatization). \n'
+              'The driver was extremely polite (he opened the doors, arrived at the agreed time,\n'
+              ' immediately offered to connect to bluetooth (if you need musical accompaniment on the road). \n'
+              'He orients himself perfectly in the area.\n'
+              ' We arrived comfortably, quickly and without any outside chatter from the driver. Thank you.\n'
+              ' We will be contact again!'
+        }
 
-    ],
-
-  },];
+  ]
+  };
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
 
       ),
-      body: ListView.builder(
-          itemCount: reviews.length,
-          itemBuilder: (context,index) => Container(
-            padding: EdgeInsets.only(left: 16,top: 24),
-            child: Column(
-              children: [
-                Text('Customer Reviews',
-                  style: TextStyle(
-                  fontSize: 25,
-                ),)
-              ],
-            ),
-          )
-      ),
+      body: SingleChildScrollView(
+        child: Container(
+              padding: EdgeInsets.only(left: 16,top: 24, right: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Customer Reviews',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),),
+                  Divider(
+                    endIndent: 220,
+                    thickness: 2,
+                    color: Colors.orange,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 24,right: 24),
+                        height: MediaQuery.of(context).size.height*.15,
+                        width: MediaQuery.of(context).size.width*.45,
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            reviews["overallrate"],
+                            style: TextStyle(
+                              fontSize: 65,
+                              color: Colors.white
+                          ),),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 24,left: 14),
+                        height: MediaQuery.of(context).size.height*.15,
+                        width: MediaQuery.of(context).size.width*.35,
+                        child: Column(
+                        //  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Fine',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22
+                            ),),
+                            Spacer(),
+                            Text("${reviews['rateN']}  reviews",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold
+                            ),),
+                            Spacer(),
+                            Container(
+                              height: MediaQuery.of(context).size.height*.045,
+                              width: MediaQuery.of(context).size.width*.45,
+                              child: RaisedButton(
+                                color: Colors.white,
+                                child:Text('write a feedback',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.orange
+                                ),
+                                ),
+                                onPressed: (){},
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    side: BorderSide(
+                                        color: Colors.orange
+                                    )
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 30),
+                    height: MediaQuery.of(context).size.height*.7,
+                    child: ListView.builder(
+                        itemCount: reviews['rate'].length,
+                        itemBuilder: (context,index) {
+                          List<String> titles = [
+                            "Driver",
+                            "Car",
+                            "Overall rating"
+                          ];
+                          String text_t = index == 0 ? titles[0] : index == 5 ? titles[1] : index == 7 ? titles[2] : " ";
+                          double h_cal = index == 0 || index == 4 || index == 6 ? 110 : 60;
+                          double rate = (reviews['rate'][index]["score"])/10;
+                          return Container(
+                            height: h_cal,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                text_t != " " ? Text(
+                                 text_t,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                ) : Container(),
+                                Text("${reviews['rate'][index]["title"]}"),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        border: Border.all(color: Colors.grey, width: 1),
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      height: 10,
+                                      width: MediaQuery.of(context).size.width*.85,
+                                      child: Stack(
+                                          children: [
+                                            FractionallySizedBox(
+                                              widthFactor: rate,
+                                              child: Container(
+                                                decoration: BoxDecoration(
+                                                  color: Colors.orange,
+                                                  borderRadius: BorderRadius.circular(10),
+                                                ),
+                                              ),
+                                            ),
+                                          ]
+                                      ),
+                                    ),
+                                    Text("${reviews['rate'][index]["score"].toInt()}"),
+
+                                  ],
+                                ),
+                              ],
+                            ),
+                          );
+            }
+        ),
+                  ),
+          ]
+      )
+    )
+    )
     );
   }
 }
