9b6c46b7631a5c3940af37514137ef5f670c55ed
Muhammadsaid22 <82633656+Muhammadsaid22@users.noreply.github.com>
2022-01-11 00:38:01 +0500

    eng uzb

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -152,6 +152,7 @@ class _HomeScreenState extends State<HomeScreen> {
   void getcities() async {
     HomeScreen.city_list = await Cities.getcities();
     HomeScreen.city_list = HomeScreen.city_list.toSet().toList();
+    print(HomeScreen.city_list);
   }
 
   void getnews () async {
