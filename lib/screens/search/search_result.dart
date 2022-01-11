43a6119f02d92c1fd9d523f8c167d9e13f83e81f
SobirovI <88648901+SobirovI@users.noreply.github.com>
2022-01-11 16:39:12 +0500

    search result back

--- lib/screens/search/search_result.dart
+++ lib/screens/search/search_result.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:easy_localization/easy_localization.dart';
@@ -9,17 +10,27 @@ import 'package:travelcars/screens/home/home_screen.dart';
 import 'package:travelcars/screens/search/details_screen.dart';
 import 'package:travelcars/screens/search/search.dart';
 import 'package:travelcars/screens/splash/splash_screen.dart';
+import 'package:http/http.dart' as http;
 
 class SearchResult extends StatefulWidget {
-  final List<dynamic> routes;
+  final Map<dynamic, dynamic> search_body;
+  final bool isCarCategory;
+  final int carCategory;
 
-  SearchResult(this.routes);
+  SearchResult({
+    this.isCarCategory = false,
+    this.search_body = const {},
+    this.carCategory = 0,
+  });
+
 
   @override
   _SearchResultState createState() => _SearchResultState();
 }
 
 class _SearchResultState extends State<SearchResult> {
+  bool isLoading = true;
+  List<dynamic> routes = [];
   List<String> icons = [
     "assets/icons/places.jpg",
     "assets/icons/big_bag.jpg",
@@ -27,6 +38,58 @@ class _SearchResultState extends State<SearchResult> {
     "assets/icons/doors.jpg",
   ];
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getSearchResult();
+  }
+
+  void getSearchResult() async {
+    Uri url = Uri.parse("${AppConfig.BASE_URL}/sort");
+    if(widget.isCarCategory) {
+      DateTime current =  DateTime.now();
+      String time = "31.12.${current.year}";
+      final result_reverse = await http.post(
+          url,
+          body: {
+            "reverse": "1",
+            "car_models[0]": "${widget.carCategory}",
+            "date_end": time,
+          }
+      );
+      final result_nonreverse = await http.post(
+          url,
+          body: {
+            "reverse": "0",
+            "car_models[0]": "${widget.carCategory}",
+            "date_end": time,
+          }
+      );
+
+      print("Non-reverse: ${jsonDecode(result_nonreverse.body)["routes"].toString()}");
+      print("Reverese: ${jsonDecode(result_reverse.body)["routes"].toString()}");
+
+      if(jsonDecode(result_nonreverse.body)["routes"].isNotEmpty) {
+        routes.addAll(jsonDecode(result_nonreverse.body)["routes"]);
+      }
+
+      if(jsonDecode(result_reverse.body)["routes"].isNotEmpty) {
+        routes.addAll(jsonDecode(result_reverse.body)["routes"]);
+      }
+    } else {
+      final response = await http.post(
+          url,
+          body: widget.search_body
+      );
+      routes = jsonDecode(response.body)["routes"];
+    }
+    print("All: ${routes.toString()}");
+
+    setState(() {
+      isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,7 +134,9 @@ class _SearchResultState extends State<SearchResult> {
       endDrawer: Drawer(
         child: SearchScreen(isDrawer: true,),
       ),
-      body: widget.routes.isEmpty ? Center(
+      body: isLoading ? Center(
+        child: CircularProgressIndicator(),
+    ) : routes.isEmpty ? Center(
         child: Text(
           "No matching results are found",
           style: TextStyle(
@@ -79,10 +144,10 @@ class _SearchResultState extends State<SearchResult> {
           ),
         ),
       ) : ListView.builder(
-          itemCount: widget.routes.length,
+          itemCount: routes.length,
           itemBuilder: (context, index) {
-            Map<String, dynamic> car = widget.routes[index]["car"];
-            List<dynamic> options = widget.routes[index]["route_options"];
+            Map<String, dynamic> car = routes[index]["car"];
+            List<dynamic> options = routes[index]["route_options"];
             List<int> icon_numbers = [];
             icon_numbers.add(car["places"]);
             icon_numbers.add(car["big_bags"]);
@@ -101,12 +166,16 @@ class _SearchResultState extends State<SearchResult> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Icon(Icons.info_outline, color: Colors.red),
+                        Icon(
+                          Icons.info_outline,
+                          color: routes[index]["status"] == 1 ? Colors.red : Colors.green,
+                        ),
                         SizedBox(width: 13),
                         Text(
-                          "Confirmation requires",
+                          routes[index]["status"] == 1 ? "Confirmation requires" : "Instant confirmation",
                           style: TextStyle(
                             fontSize: 20,
+                            color: routes[index]["status"] == 1 ? Colors.red : Colors.green,
                           ),
                         ),
                       ],
@@ -224,7 +293,7 @@ class _SearchResultState extends State<SearchResult> {
                     margin: EdgeInsets.all(5),
                     child: ListTile(
                       title: Text(
-                        "${widget.routes[index]["title"]}",
+                        "${routes[index]["title"]}",
                         textAlign: TextAlign.center,
                         style: TextStyle(
                             fontSize: 19,
@@ -239,7 +308,7 @@ class _SearchResultState extends State<SearchResult> {
                             Icon(Icons.watch_later_outlined, color: Colors.orange,),
                             SizedBox(width: 8),
                             Text(
-                              "${widget.routes[index]["date"]}",
+                              "${routes[index]["date"]}",
                               style: TextStyle(
                                 fontSize: 17,
                               ),
@@ -268,7 +337,7 @@ class _SearchResultState extends State<SearchResult> {
                     padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
                     alignment: Alignment.center,
                     child: Text(
-                      "${(widget.routes[index]["price"] * app_kurs).toStringAsFixed(2)} ${SplashScreen.kurs}",
+                      "${(routes[index]["price"] * app_kurs).toStringAsFixed(2)} ${SplashScreen.kurs}",
                       style: TextStyle(
                           fontSize: 25,
                           color: Colors.black,
@@ -280,7 +349,7 @@ class _SearchResultState extends State<SearchResult> {
                     onTap: () {
                       Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => DetailScreen(widget.routes[index]))
+                          MaterialPageRoute(builder: (context) => DetailScreen(routes[index]))
                       );
                     },
                     child: Container(
