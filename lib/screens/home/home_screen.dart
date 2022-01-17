e7078077005e673b2f2ab2ba8f89a094903836a0
SobirovI <88648901+SobirovI@users.noreply.github.com>
2022-01-17 17:52:17 +0500

    home screen fix

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -465,13 +465,17 @@ class _HomeScreenState extends State<HomeScreen> {
                       Positioned(
                         bottom: 12.0,
                         child: Container(
+                          height: 70,
+                          alignment: Alignment.bottomLeft,
+                          width: MediaQuery.of(context).size.width * .8,
                           padding: EdgeInsets.symmetric(
                               vertical: 10.0, horizontal: 20.0),
                           child: Text(
                             "${item["name"]}",
+                            maxLines: 2,
                             style: TextStyle(
                               color: Colors.white,
-                              fontSize: 25.0,
+                              fontSize: 20.0,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
@@ -499,6 +503,9 @@ class _HomeScreenState extends State<HomeScreen> {
                 );
               }).toList(),
             ),
+            SizedBox(
+              height: 15,
+            ),
             Container(
               padding: EdgeInsets.only(left: 12, top: 12, bottom: 6),
               child: Text(
@@ -511,69 +518,73 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             Container(
-              height: 370.0,
+              height: 320.0,
               margin: EdgeInsets.only(left: 15, right: 15, top: 15),
               child: GridView.builder(
                   physics: NeverScrollableScrollPhysics(),
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2,
-                      crossAxisSpacing: 15.0,
-                      mainAxisSpacing: 15.0
+                      crossAxisSpacing: 10.0,
+                      mainAxisSpacing: 10.0
                   ),
                   itemCount: 4,
+                  padding: EdgeInsets.zero,
                   itemBuilder: (BuildContext ctx, index) {
                     return GestureDetector(
                       onTap: (){
                         Navigator.push(context,MaterialPageRoute(
                             builder: (_) => CarCategory(carslist[index]["name"], carslist[index]["meta_url"])));
                       },
-                      child: Card(
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                        elevation: 4,
-                        child: Stack(
-                          children: [
-                            Container(
-                              height: 140,
-                              width: MediaQuery.of(context).size.width * .43,
-                              child: Image.network("${AppConfig.image_url}/car-types/${carslist[index]["image"]}"),
-                            ),
-                            Positioned(
-                              top: 10,
-                              right: 10,
-                              child: Container(
-                                  height: 25,
-                                  width: 25,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5.0),
-                                    color: Color.fromRGBO(239, 127, 26, 1),
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      "${carslist[index]["quantity"]}",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 17
-                                      ),
-                                    ),
-                                  )
+                      child: Container(
+                        height: 100,
+                        child: Card(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                          elevation: 4,
+                          child: Stack(
+                            children: [
+                              Container(
+                                height: 120,
+                                width: MediaQuery.of(context).size.width * .43,
+                                child: Image.network("${AppConfig.image_url}/car-types/${carslist[index]["image"]}"),
                               ),
-                            ),
-                            Positioned(
-                              bottom: 20,
-                              left: 5,
-                              child: Container(
-                                width: MediaQuery.of(context).size.width * .4,
-                                alignment: Alignment.center,
-                                child: Text(
-                                  "${carslist[index]["name"]}",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      fontSize: 17
-                                  ),
+                              Positioned(
+                                top: 10,
+                                right: 10,
+                                child: Container(
+                                    height: 25,
+                                    width: 25,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                      color: Color.fromRGBO(239, 127, 26, 1),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        "${carslist[index]["quantity"]}",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 17
+                                        ),
+                                      ),
+                                    )
                                 ),
                               ),
-                            )
-                          ],
+                              Positioned(
+                                bottom: 15,
+                                left: 5,
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width * .4,
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "${carslist[index]["name"]}",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 14
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     );
@@ -606,6 +617,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ),
+            ),
+            SizedBox(
+              height: 20,
             ),
             Container(
               padding: EdgeInsets.only(left: 12, bottom: 6),
@@ -700,7 +714,7 @@ class CarsCard extends StatelessWidget {
       child: Stack(
         children: [
           Container(
-            height: 140,
+            height: 130,
             width: MediaQuery.of(context).size.width * .43,
             child: Image.asset(image),
           ),
